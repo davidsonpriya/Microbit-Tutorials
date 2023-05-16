a18abe3ab1d7dc6e0d4c537ab50d6a95074370e3
davidsonpriya <90389839+davidsonpriya@users.noreply.github.com>
2023-05-16 23:36:05 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+let LED = 0
+let item = neopixel.create(DigitalPin.P1, 7, NeoPixelMode.RGB)
+basic.forever(function () {
+    LED = pins.map(
+    input.soundLevel(),
+    0,
+    70,
+    0,
+    7
+    )
+    item.clear()
+    item.range(0, LED).showRainbow(1, 255)
+    item.show()
+})
